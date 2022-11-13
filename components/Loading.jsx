2d8645e93d9316b2157171cebd8eca026fc0bc6f
Midokhalil1 <midokhalil22@gmail.com>
2022-11-13 14:20:08 -0500

    loadind

---
+++ components/Loading.jsx
@@ -0,0 +1,15 @@
+import { View } from "react-native"
+
+const Loading = () => {
+    return (
+        <View style={Style.container}>
+            <Text>
+                Loading...
+            </Text>
+
+
+        </View>
+    );
+            
+        }
+    
